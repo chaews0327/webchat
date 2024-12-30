8aad1d47d05ca711ac23227046d8c62952f5a561
chaews0327 <chaews0327@gmail.com>
2024-12-30 17:30:42 +0900

    Init: Tutorial Code

---
+++ main.py
@@ -0,0 +1,50 @@
+import logging
+
+import jinja2
+
+import aiohttp_jinja2
+from aiohttp import web
+from views import index, ws_key
+
+import os
+
+
+async def init_app():
+
+    app = web.Application()
+
+    app[ws_key] = {}
+
+    app.on_shutdown.append(shutdown)
+
+    aiohttp_jinja2.setup(
+        app, loader=jinja2.FileSystemLoader(os.getcwd() + "/template"))
+
+    app.router.add_get('/', index)
+
+    return app
+
+
+async def shutdown(app):
+    for ws in app[ws_key].values():
+        await ws.close()
+    app[ws_key].clear()
+
+
+async def get_app():
+    """Used by aiohttp-devtools for local development."""
+    import aiohttp_debugtoolbar
+    app = await init_app()
+    aiohttp_debugtoolbar.setup(app)
+    return app
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    app = init_app()
+    web.run_app(app)
+
+
+if __name__ == '__main__':
+    main()
