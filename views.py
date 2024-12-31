634eb2895e31478b40305f1d9694179911c78f67
chaews0327 <chaews0327@gmail.com>
2024-12-31 10:40:03 +0900

    Refactor: Refactor WebSocket handling and user ID management

--- views.py
+++ views.py
@@ -15,22 +15,25 @@ def get_random_name():
     return fake.name()
 
 
-async def index(request):
+def set_user_id(request, response):
+    user_id = get_random_name()
+    response.set_cookie('user_id', user_id, max_age=60*60*24)
+    return response
+
+
+async def handle_websocket(request, user_id):
     ws_current = web.WebSocketResponse()
     ws_ready = ws_current.can_prepare(request)
     if not ws_ready.ok:
-        return aiohttp_jinja2.render_template('index.html', request, {})
-
+        return aiohttp_jinja2.render_template('index.html', request, {})   
+    
     await ws_current.prepare(request)
-
-    name = get_random_name()
-    log.info('%s joined.', name)
-
-    await ws_current.send_json({'action': 'connect', 'name': name})
-
+    await ws_current.send_json({'action': 'connect', 'name': user_id})
+    
     for ws in request.app[ws_key].values():
-        await ws.send_json({'action': 'join', 'name': name})
-    request.app[ws_key][name] = ws_current
+        await ws.send_json({'action': 'join', 'name': user_id})
+        
+    request.app[ws_key][user_id] = ws_current
 
     while True:
         msg = await ws_current.receive()
@@ -39,13 +42,26 @@ async def index(request):
             for ws in request.app[ws_key].values():
                 if ws is not ws_current:
                     await ws.send_json(
-                        {'action': 'sent', 'name': name, 'text': msg.data})
+                        {'action': 'sent', 'name': user_id, 'text': msg.data})
         else:
             break
-
-    del request.app[ws_key][name]
-    log.info('%s disconnected.', name)
+        
+    if user_id in request.app[ws_key]:
+        del request.app[ws_key][user_id]
+    log.info('%s disconnected.', user_id)
     for ws in request.app[ws_key].values():
-        await ws.send_json({'action': 'disconnect', 'name': name})
+        await ws.send_json({'action': 'disconnect', 'name': user_id})
 
     return ws_current
+
+
+async def index(request):  
+    user_id = request.cookies.get('user_id')
+    
+    if not user_id:
+        response = web.Response()
+        user_id = set_user_id(request, response)
+        await handle_websocket(request, user_id)
+        return response
+
+    return await handle_websocket(request, user_id)
