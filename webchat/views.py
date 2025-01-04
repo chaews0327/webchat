32eb15d876ae1fb08a0e7ce93376d27b015322f8
chaews0327 <chaews0327@gmail.com>
2025-01-05 04:12:42 +0900

    Update: change user ID management to use Redis

--- webchat/views.py
+++ webchat/views.py
@@ -4,12 +4,20 @@ from chat_service import manage_websocket_connection
 
 
 async def index(request):
+    redis = request.app['redis']
     session = await aiohttp_session.get_session(request)
     user_id = session.get('user_id')
 
     if not user_id:
-        # user_id = Faker().name()
-        user_id = uuid().hex
-        session['user_id'] = str(user_id)[:4]
+        user_id = str(uuid().hex)[:4]
+        
+        while await redis.sismember('user_ids', user_id):
+            user_id = str(uuid().hex)[:4]
+            
+        await redis.sadd('user_ids', user_id)
+        await redis.expire(user_id, 3600)
+        
+        session['user_id'] = user_id
+        await redis.setex(f"session:{user_id}", 3600, "active")
 
     return await manage_websocket_connection(request, user_id)
